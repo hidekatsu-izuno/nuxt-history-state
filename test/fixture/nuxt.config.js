da1edae5ecf90488d51fcc61cadc7b3f40e1a945
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2019-12-28 16:36:57 +0900

    Improve test settings

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,17 +1,8 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    mode: 'spa',
-    dev: true,
-    rootDir: path.resolve(__dirname, '../..'),
-    buildDir: path.resolve(__dirname, '.nuxt'),
-    srcDir: __dirname,
-    render: {
-        resourceHints: false
-    },
+    rootDir: __dirname,
     modules: [
-        {
-            handler: require('../..')
-        }
+        { handler: require('../../') }
     ]
 }
--- test/nuxt-history-state.test.js
+++ test/nuxt-history-state.test.js
@@ -1,43 +1,17 @@
-import { Nuxt, Builder } from 'nuxt-edge';
-import config from './fixture/nuxt.config';
-
-const port = 3001;
-const url = path => `http://localhost:${port}${path}`;
-jest.setTimeout(60000);
-
-let nuxt;
-
-const setupNuxt = async (config) => {
-    nuxt = new Nuxt(config);
-  
-    await new Builder(nuxt).build();
-    await nuxt.listen(port);
-}
+const { setup, loadConfig, get, url } = require('@nuxtjs/module-test-utils');
 
 describe('reloadable:true', () => {
+    let nuxt;
+
     beforeAll(async () => {
-        await setupNuxt(config);
-    });
-    
+      ({ nuxt } = (await setup(loadConfig(__dirname))));
+    }, 60000)
+  
     afterAll(async () => {
-        await nuxt.close();
+      await nuxt.close();
     });
 
     test('run nuxt', async () => {
-        await nuxt.renderAndGetWindow(url('/page1'));
-    });
-});
-
-describe('reloadable:false', () => {
-    beforeAll(async () => {
-        await setupNuxt(config);
-    });
-    
-    afterAll(async () => {
-        await nuxt.close();
-    });
-
-    test('run nuxt', () => {
-
+        const window = await nuxt.renderAndGetWindow(url('/page1'));
     });
 });
